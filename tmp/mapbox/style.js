7f9359fedf8fb434203503334572f661e18710a2
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2019-10-04 13:09:01 +0100

    Absolute URL

--- tmp/mapbox/style.js
+++ tmp/mapbox/style.js
@@ -11,16 +11,18 @@ var styleJSON = {
     "light": {
         "intensity": 0.2
     },
-    "sources": {        "test_0": {
+    "sources": {
+        "test_0": {
             "type": "image",
-            "url": "./data/test_0.png",
+            "url": "https://tom.chadw.in/tmp/data/test_0.png",
             "coordinates": [
                 [-2, 54],
                 [-2, 54],
                 [-2, 54],
                 [-2, 54]
             ]
-        }},
+        }
+    },
     "sprite": "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/sprites",
     "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
     "layers": [
@@ -32,13 +34,14 @@ var styleJSON = {
                 "background-color": "#ffffff"
             }
         },
-{
-"id": "simple-tiles",
-"type": "raster",
-"source": "test_0",
-"minzoom": 0,
-"maxzoom": 22
-}],
+        {
+            "id": "simple-tiles",
+            "type": "raster",
+            "source": "test_0",
+            "minzoom": 0,
+            "maxzoom": 22
+        }
+    ],
     "created": "2018-05-11T11:38:48.884Z",
     "id": "cjh1w236f0tj22sl87sm547vt",
     "modified": "2018-05-26T21:01:47.000Z",
