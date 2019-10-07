fd6de57c28fd24548a14c1487456c286be8ac80f
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2019-10-07 11:54:24 +0100

    New raster test

--- tmp/mapbox/style.js
+++ tmp/mapbox/style.js
@@ -1,12 +1,7 @@
 
 var styleJSON = {
     "version": 8,
-    "name": "OS Outdoor",
-
-    "center": [
-        -1.445462913521851,
-        50.924985957591986
-    ],
+    "name": "qgis2web export",
     "pitch": 0,
     "light": {
         "intensity": 0.2
@@ -16,13 +11,12 @@ var styleJSON = {
             "type": "image",
             "url": "./data/test_0.png",
             "coordinates": [
-                [-2.1109019480555116, 54.9795677650124688],
-                [-2.0952682828989335, 54.9795677650124688],
-                [-2.0952682828989335, 54.9705596140216741],
-                [-2.1109019480555116, 54.9705596140216741]
+                [-2, 54],
+                [-2, 54],
+                [-2, 54],
+                [-2, 54]
             ]
-        }
-    },
+        }},
     "sprite": "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/sprites",
     "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
     "layers": [
@@ -35,17 +29,10 @@ var styleJSON = {
             }
         },
         {
-            "id": "simple-tiles",
+            "id": "test_0",
             "type": "raster",
             "source": "test_0",
             "minzoom": 0,
             "maxzoom": 22
-        }
-    ],
-    "created": "2018-05-11T11:38:48.884Z",
-    "id": "cjh1w236f0tj22sl87sm547vt",
-    "modified": "2018-05-26T21:01:47.000Z",
-    "owner": "Ordnance Survey",
-    "visibility": "public",
-    "draft": false
+        }],
 }
