cc362a148aec5c281ea54b914a43031c110e6adc
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2019-10-21 17:54:36 +0100

    Toned down

--- maps/halloween/mapbox/style.js
+++ maps/halloween/mapbox/style.js
@@ -8,7 +8,7 @@ var styleJSON = {
     },
     "sources": {
         "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211": {
-            "url": "https://api.maptiler.com/tiles/v3/tiles.json?key=dqqigsTMgVQt82L8d0a3",
+            "url": "https://api.maptiler.com/tiles/v3/tiles.json?key=6irhAXGgsi8TrIDL0211",
             "type": "vector"
         }},
     "sprite": "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/sprites",
@@ -19,7 +19,7 @@ var styleJSON = {
             "type": "background",
             "layout": {},
             "paint": {
-                "background-color": "#a2a2a2"
+                "background-color": "#bcbcbc"
             }
         },
         {
@@ -28,7 +28,7 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "landuse",
             "layout": {},
-            "paint": {'fill-opacity': 1.0, 'fill-color': '#e18761'}
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#bba79a'}
         },
         {
             "id": "lyr_landcover_1_0",
@@ -36,7 +36,7 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "landcover",
             "layout": {},
-            "paint": {'fill-opacity': 1.0, 'fill-color': '#ee9271'}
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#b2b8ab'}
         },
         {
             "id": "lyr_waterway_2_0",
@@ -44,7 +44,7 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "waterway",
             "layout": {},
-            "paint": {'line-width': 0.982677165366, 'line-opacity': 1.0, 'line-color': '#333333'}
+            "paint": {'line-width': 0.982677165366, 'line-opacity': 1.0, 'line-color': '#969696'}
         },
         {
             "id": "lyr_water_3_0",
@@ -52,7 +52,7 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "water",
             "layout": {},
-            "paint": {'fill-opacity': 1.0, 'fill-color': '#333333'}
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#a3a3a3'}
         },
         {
             "id": "lyr_building_4_0",
@@ -60,7 +60,7 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "building",
             "layout": {},
-            "paint": {'fill-opacity': 1.0, 'fill-color': '#e5510c'}
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#af8f79'}
         },
         {
             "id": "lyr_transportation_6_0",
@@ -68,30 +68,14 @@ var styleJSON = {
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "transportation",
             "layout": {},
-            "paint": {'line-width': 0.982677165366, 'line-opacity': 1.0, 'line-color': '#666666'}
-        },
-        {
-            "id": "lyr_transportation_name_8_0",
-            "type": "symbol",
-            "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
-            "source-layer": "transportation_name",
-            "layout": {'text-offset': [0.0, 0.0], 'text-field': ['get', 'name_en'], 'text-size': 10.0, 'text-font': ['Open Sans Regular']},
-            "paint": {'text-color': '#000000'}
-        },
-        {
-            "id": "lyr_water_name_9_0",
-            "type": "symbol",
-            "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
-            "source-layer": "water_name",
-            "layout": {'text-offset': [0.0, 0.0], 'text-field': ['get', 'name'], 'text-size': 10.0, 'text-font': ['Open Sans Regular']},
-            "paint": {'text-color': '#000000'}
+            "paint": {'line-width': 0.982677165366, 'line-opacity': 1.0, 'line-color': '#969696'}
         },
         {
             "id": "lyr_place_11_0",
             "type": "symbol",
             "source": "httpsapimaptilercomtilesv3tilesjsonkey6irhAXGgsi8TrIDL0211",
             "source-layer": "place",
-            "layout": {'text-offset': [0.0, 0.0], 'text-field': ['get', 'name'], 'text-size': 10.0, 'text-font': ['Open Sans Regular']},
-            "paint": {'text-halo-width': 1.0, 'text-halo-color': '#d5d5d5', 'text-color': '#000000'}
+            "layout": {'text-offset': [0.0, 0.0], 'text-field': ['get', 'name'], 'text-size': 12.0, 'text-font': ['Open Sans Regular']},
+            "paint": {'text-color': '#5f6868'}
         }],
 }
