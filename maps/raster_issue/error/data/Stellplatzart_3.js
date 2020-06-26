cefbd165002ac9f5942221602fbc2a7b6b9818a3
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2020-06-26 13:31:37 +0100

    Raster error map

---
+++ maps/raster_issue/error/data/Stellplatzart_3.js
@@ -0,0 +1,21 @@
+var json_Stellplatzart_3 = {
+"type": "FeatureCollection",
+"name": "Stellplatzart_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Art": "Behindert", "Nutzung": "Stellplatz" }, "geometry": { "type": "Point", "coordinates": [ 9.906385612359555, 53.633975828771696 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Behindert", "Nutzung": "Stellplatz" }, "geometry": { "type": "Point", "coordinates": [ 9.906394785183512, 53.633941802295816 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Behindert", "Nutzung": "Stellplatz" }, "geometry": { "type": "Point", "coordinates": [ 9.906401818772352, 53.633914583425586 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.90624302304256, 53.633756416875322 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906354602375657, 53.6337732281455 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906366514789294, 53.63368937744292 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906219021775975, 53.633667405463811 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906097967235414, 53.633599504199047 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906229444292128, 53.633549609178161 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906328663781069, 53.633629447658613 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906402370323249, 53.633514340630711 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906500819649175, 53.633348820089537 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Fahrrad", "Nutzung": "Fahrradständer" }, "geometry": { "type": "Point", "coordinates": [ 9.906465461592703, 53.633281766622822 ] } },
+{ "type": "Feature", "properties": { "id": null, "Art": "Behindert", "Nutzung": "Stellplatz" }, "geometry": { "type": "Point", "coordinates": [ 9.906326773909024, 53.633838815123724 ] } }
+]
+}
